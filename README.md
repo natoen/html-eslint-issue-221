14c3aaebf78fc2ba644a70bd37b1a79bfed1cf63
hiroshi.c.matsuda <hiroshi.c.matsuda@rakuten.com>
2024-10-04 04:56:20 +0900

    feat: add issue in a reproducible fashion

---
+++ README.md
@@ -0,0 +1,7 @@
+# html-eslint [Issue 221](https://github.com/yeonjuan/html-eslint/issues/221)
+
+This is for showing in a reproducible fashion the submitted issue 221 for html-eslint.
+
+- do a `npm i` with a Node.js v20.17 or just use Volta
+- `node index.js`
+- you should see an error `Error: ESLint configuration in BaseConfig is invalid: Unexpected top-level property "languageOptions".`
---
+++ index.js
@@ -0,0 +1,27 @@
+const { ESLint } = require("eslint");
+const html = require("eslint-plugin-html");
+const htmlEslint = require('@html-eslint/eslint-plugin');
+const PARSER_HTML = require('@html-eslint/parser');
+
+(async function() {
+  const eslint = new ESLint({
+    baseConfig: {
+      languageOptions: {
+        parser: PARSER_HTML
+      },
+    },
+    plugins: {
+      html,
+      "@html-eslint": htmlEslint,
+    },
+  });
+
+  const lintResult = (await eslint.lintText(sourceCode, {
+    // this is needed to work but this file does not exist
+    filePath: "placeholder.html"
+  }))[0];
+  console.log(results);
+})().catch((error) => {
+  process.exitCode = 1;
+  console.error(error);
+});
